f7f32dc916f4401e9f3a131f86ea8af253855f00
AlexanderBulldog <s.martynenko@bk.ru>
2023-10-21 13:49:31 +0300

    ver1

--- 5.3.py
+++ 5.3.py
@@ -1,14 +1,15 @@
 def bayes_theorem(p_a, p_b_given_a, p_b_given_not_a):
     # Вычисляем P(not A) - вероятность несобытия A
     p_not_a = 1 - p_a
-    
+
     # Вычисляем полную вероятность P(B)
     p_b = (p_b_given_a * p_a) + (p_b_given_not_a * p_not_a)
-    
+
     # Вычисляем P(A|B) - условную вероятность A при условии B
     p_a_given_b = (p_b_given_a * p_a) / p_b
-    
+
     return p_a_given_b
+
 
 # Пример использования функции
 p_a = 5/12  # Априорная вероятность события A
